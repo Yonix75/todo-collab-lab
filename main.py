4b7038fe2c40032a4836fa0ede758c4604d73daa
NivBar2000 <bar.niv2000@gmail.com>
2026-03-28 20:33:17 +0300

    B used the new function and added new task

--- main.py
+++ main.py
@@ -1,4 +1,4 @@
-from todo import add_task, list_tasks
+from todo import add_task, list_tasks,remove_last_task
 
 add_task("Initial task by A")
 
@@ -9,3 +9,6 @@ for t in tasks:
 add_task("B: buy groceries")
 
 add_task("A: clean room")
+
+remove_last_task()
+add_task("B: replaced last task")
