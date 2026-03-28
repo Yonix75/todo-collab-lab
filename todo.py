164538d9168f9368ff3f6db95828b3bb6da32c21
Yoni <contact.yonibena@gmail.com>
2026-03-28 20:31:25 +0300

    A add remove last task

--- todo.py
+++ todo.py
@@ -5,3 +5,16 @@ def add_task(task):
 def list_tasks():
     with open("tasks.txt", "r") as f:
         return f.readlines()
+
+def remove_last_task():
+    with open("tasks.txt", "r") as f:
+        lines = f.readlines()
+    with open("tasks.txt", "w") as f:
+        f.writelines(lines[:-1])    
+        
+        
+def remove_last_task():
+    with open("tasks.txt", "r") as f:
+        lines = f.readlines()
+    with open("tasks.txt", "w") as f:
+        f.writelines(lines[:-1])        
